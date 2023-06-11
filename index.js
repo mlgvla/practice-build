4fb439eb880e4ba6a9598aa6b7c60635ee0019dc
Monica Gerard <mlgvla@gmail.com>
2023-06-11 01:24:44 -0400

    completes cuisine dropdown

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+// Elements
+const cuisineSelect = document.querySelector("#cuisines")
+const categorySelect = document.querySelector("#category")
+
+// Function Calls
+getCuisines()
+
+// Populate Dropdowns
+function getCuisines() {
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+    .then(r => r.json())
+    .then(cuisines => renderCuisineOptions(cuisines.meals))
+    .catch(error => alert(error))
+}
+
+function renderCuisineOptions(cuisines) {
+    cuisines.forEach(cuisine => {
+        const option = document.createElement("option")
+        option.value = cuisine.strArea
+        option.textContent = cuisine.strArea
+        cuisineSelect.append(option)
+        console.log(option)
+    });
+}
